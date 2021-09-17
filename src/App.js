709d004ae6e5162bb235fe35ce653f79b9a424b5
wallacesfos <wallacesfo@hotmail.com>
2021-09-17 16:53:58 -0300

    Feito

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
     name: yup.string().required('Nome obrigatório'),
     email: yup.string().required('email obrigatório').email('email invalido'),
     confemail: yup.string().oneOf([yup.ref('email'), null], 'Emails devem ser iguais'),
-    password: yup.string().required('Senha obrigatório').matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*)(?=.*[@$!%*?&])[A-Za-z@$!%*?&]{8,}$", 'Senha deve ser forte'),
+    password: yup.string().required('Senha obrigatório').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+    , 'Senha deve ser forte'),
     passwordConfirmation: yup.string()
      .oneOf([yup.ref('password'), null], 'Senhas devem ser iguais')
   })
@@ -27,14 +28,23 @@ function App() {
 
   return (
     <div className="App">
+    
       <header className="App-header">
+        <ul>
+          {<li>{errors.user?.message}</li>}
+          {<li>{errors.name?.message}</li>}
+          {<li>{errors.email?.message}</li>}
+          {<li>{errors.confemail?.message}</li>}
+          {<li>{errors.password?.message}</li>}
+          {<li>{errors.passwordConfirmation?.message}</li>}
+        </ul>
         <form onSubmit={handleSubmit(onSubmitFunction)}>
-          <input placeholder={errors.user?.message || 'Nome de usuário'} className={errors.user && 'vermelho' } {...register('user')}/>
-          <input placeholder={errors.name?.message || "Nome completo"} className={errors.name && 'vermelho' } {...register('name')}/>
-          <input placeholder={errors.email?.message || "Email"} className={errors.email && 'vermelho' } {...register('email')}/>
-          <input placeholder={errors.confemail?.message ||"Confirme o email"} className={errors.email && 'vermelho' } {...register('confemail')}/>
-          <input type="password" placeholder={errors.password?.message ||"Senha"} className={errors.password && 'vermelho' } {...register('password')}/>
-          <input type="password" placeholder={errors.passwordConfirmation?.message ||"Confirme senha"}  className={errors.passwordConfirmation && 'vermelho' } {...register('passwordConfirmation')}/>
+          <input type="text"  maxlength="15" placeholder='Nome de usuário' className={errors.user && 'vermelho' } {...register('user')}/>
+          <input type="text"  maxlength="80" placeholder="Nome completo" className={errors.name && 'vermelho' } {...register('name')}/>
+          <input placeholder="Email" className={errors.email && 'vermelho' } {...register('email')}/>
+          <input placeholder="Confirme o email" className={errors.email && 'vermelho' } {...register('confemail')}/>
+          <input type="password" placeholder="Senha" maxlength="30" className={errors.password && 'vermelho' } {...register('password')}/>
+          <input type="password" placeholder="Confirme senha" maxlength="30" className={errors.passwordConfirmation && 'vermelho' } {...register('passwordConfirmation')}/>
           <button type="submit" className="cadastrar">Cadastrar</button>
         </form>
       </header>
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+
+export default props => {
+    const {erros} = props
+    return(
+        <div>
+            {erros.user && erros.user}
+
+        </div>
+    )
+}
